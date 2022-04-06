a426b302b9b3e4c362cd1d94a4ae9463d5c6856f
MarineGueguen <96696004+MarineGueguen@users.noreply.github.com>
2022-04-06 14:17:35 +0200

    Update products.php

--- public/products.php
+++ public/products.php
@@ -4,3 +4,6 @@
 require_once __DIR__ . '/../config/twig.php';
 
 $products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
+
+
+echo $twig->render('products.html.twig', ['products' => $products]);
